c744777ebeb4d083328de1b1fee5e6271346e649
njuWenHunzi <77879687+njuWenHunzi@users.noreply.github.com>
2021-01-23 19:13:16 +0800

    爬虫，在b站以关键词为“疫情”的页面爬取视频的BV号

--- Spider.py
+++ Spider.py
@@ -9,7 +9,7 @@ FindLink=re.compile(r'href="//www.bilibili.com/video/(.*?)?from=search" target="
 head={'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
 for j in range(1,5):
     response = urllib.request.Request(
-        'https://search.bilibili.com/all?keyword=%E7%96%AB%E6%83%85&order=totalrank&duration=0&tids_1=0&page='+str(j), headers=head)
+        'https://search.bilibili.com/all?keyword=%E7%96%AB%E6%83%85&order=totalrank&duration=0&tids_1=0&page='+str(j), headers=head) #B站搜索“疫情”页面
     r = urllib.request.urlopen(response)
     html = r.read().decode("utf-8")
     soup = BeautifulSoup(html, 'html.parser')
@@ -23,25 +23,3 @@ for j in range(1,5):
                 i += 2
             break
     print('--------------------------------------------------') #分割线
-    # print(item)
-
-
-
-# r=requests.get('https://api.bilibili.com/x/v2/reply?jsonp=jsonp&pn=1&type=12&oid=4620519')
-# data=json.loads(r.text)
-# # pprint.pprint(data['data']['replies'])
-# workbook=xlwt.Workbook(encoding='utf-8')
-# worksheet=workbook.add_sheet('B站评论')
-# worksheet.write(0,0,'评论')
-# MainReplyNum=1
-#
-# for i in data['data']['replies']:
-#     pprint.pprint(i)
-#     worksheet.write(MainReplyNum,0,i['content']['message'])
-#     MainReplyNum+=1
-#     for j in i['replies']:
-#         pprint.pprint(j['content']['message'])
-#         worksheet.write(MainReplyNum,0,j['content']['message'])
-#         MainReplyNum += 1
-#
-# workbook.save('B站评论爬取.xls')
