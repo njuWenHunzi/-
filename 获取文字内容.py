0dde0bb03457bb63ab7b7b149af923977e08c218
njuWenHunzi <77879687+njuWenHunzi@users.noreply.github.com>
2021-01-23 18:52:17 +0800

    Update 获取文字内容.py

--- 获取文字内容.py
+++ 获取文字内容.py
@@ -20,30 +20,13 @@ def read_excel():
             a2[i]=int(Sheet.row_values(i+1)[0].split(' ')[1])
         print(a1)
         print(a2)
-        # writebook=xlsxwriter.Workbook('C:/Users/文/Desktop/123.xlsx')
-        # tb=writebook.add_worksheet('sheet1')
-        # tb.write_string(0,0,"first")
+      
         sheet_name = workBook.sheet_names()[0]
         sheet = workBook.sheet_by_index(0)
-        # row=sheet.row_values(0)
-        # nrows=sheet.nrows;
-        # yiqingnum=0
-        # zidian={"疫情","新增","患者"}
-        # for i in zidian:
-        #     obj=i
-        #     for num in range(1, nrows):
-        #         row = sheet.row_values(num)
-        #         if (re.search(obj, row[2])):
-        #             yiqingnum += 1
-        #     print(obj, "出现次数=", yiqingnum)
-        #
+     
         for num in range(0,len(a1)):
             row = sheet.row_values(a1[num])
             Allstr=Allstr+row[a2[num]+3]
-            # Allstr = Allstr + row[3] + '\n' + "评论     " + '编号' + str(num) + ' 1 ' + row[
-            #     4] + '\n            ' + '编号' + str(num) + ' 2 ' + row[5] + '\n            ' + '编号' + str(num) + ' 3 ' + \
-            #          row[6] + '\n            ' + '编号' + str(num) + ' 4 ' + row[7] + '\n            ' + '编号' + str(
-            #     num) + ' 5 ' + row[8] + '\n\n'
         ji+=1
     f=open("C:/Users/文/Desktop/微博消极评论.txt",'a',encoding='utf-8')
     f.write(Allstr)
